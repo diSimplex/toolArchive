3d5ce4470e85c78f7069cec4952f9b8770950548
Stephen Gaito <stephen@perceptisys.co.uk>
2020-09-16 12:03:07 +0000

    refactored cnClient into browserApps split across each interface and main server which is then packaged into a browserApp for cnNursery and cnTypeSetter

--- cnNursery/main.go
+++ cnNursery/main.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate ../webserver/copyBrowserApp
+//go:generate ls -R browserApp
+
 package main
 
 import (
--- natsServer/natsServer.go
+++ natsServer/natsServer.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 // The connection to the cnNursery nats server.
 //
 package webserver
